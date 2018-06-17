62b0ceab6b8fa0572620ba9d3ae6d80c14df2e3c
Wojtek Erbetowski <wojtek@erbetowski.pl>
2018-06-17 14:55:27 +0200

    Removed pyc

--- todolist/settings.py
+++ todolist/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'todolist',
-        'USER':'root',
-        'PASSWORD':'123456',
-        'HOST':'',
-        'PORT':''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'todolist_db',
     }
 }
 
